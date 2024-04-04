dae65a757e5ffe18d5cc478df83c76090907c2e1
Mihalkevitc <144014512+Mihalkevitc@users.noreply.github.com>
2024-04-04 14:00:15 +0300

    lab11

---
+++ lab11/src/main/java/com/example/lab11/lab11Application.java
@@ -0,0 +1,13 @@
+package com.example.lab11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lab11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(lab11Application.class, args);
+	}
+
+}
---
+++ lab11/src/test/java/ru/mirea/ikbo20/lab11/lab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo20.lab11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class lab11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
