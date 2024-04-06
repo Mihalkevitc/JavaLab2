47e081eed77a8be0c8a565089c13bb99072e6aa0
Mihalkevitc <144014512+Mihalkevitc@users.noreply.github.com>
2024-04-06 14:12:53 +0300

    lab12

---
+++ lab12/src/main/java/org/example/lab12Application.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lab12Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(lab12Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+    }
+}
